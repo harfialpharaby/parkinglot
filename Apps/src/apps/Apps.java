33240f4b97b4faf7ccf1c2c14269cfe4d2ab658c
Dataon <Dataon@LAPBLZ018.ASJ211304-0000401>
2019-07-06 17:57:52 +0700

    fixing function on car class

--- Apps/src/apps/Apps.java
+++ Apps/src/apps/Apps.java
@@ -24,17 +24,43 @@ public class Apps {
     public static void main(String[] args) throws IOException{
         // TODO code application logic here
         Scanner myObj = new Scanner(System.in);
-        String Commands;
         
         // membuat objek player
         Car parking = new Car();
 
-        // Enter Commands and press Enter
-        Commands = myObj.nextLine();
-        
-        if(Commands.contains(" ")){
-            Commands = Commands.substring(0, Commands.indexOf(" "));
-            System.out.println("Commands is: " + Commands + " and length: "+ Commands.length());
-        }
+        String Commands=null;
+        do{
+            // Enter Commands and press Enter
+            Commands = myObj.nextLine();
+            String[] splitCommand = Commands.split(" ");
+            switch(splitCommand[0]){
+                case "create_parking_lot":
+                    parking.initLot(splitCommand[1]);
+                    break;
+                case "park":
+                    parking.park(splitCommand[1], splitCommand[2]);
+                    break;
+                case "leave":
+                    parking.leave(splitCommand[1]);
+                    break;
+                case "status":
+                    parking.status();
+                    break;
+                case "registration_numbers_for_cars_with_colour":
+                    parking.getNoByColor(splitCommand[1]);
+                    break;
+                case "slot_numbers_for_cars_with_colour":
+                    parking.getSlotByColor(splitCommand[1]);
+                    break;
+                case "slot_number_for_registration_number":
+                    parking.getSlotByNo(splitCommand[1]);
+                    break;
+                case "exit":
+                    break;
+                default:
+                    System.out.println(splitCommand[0]+" command doesn't exist!");
+                    break;
+            }
+        }while(!Commands.contains("exit"));
     }
 }
--- Apps/src/apps/Car.java
+++ Apps/src/apps/Car.java
@@ -11,31 +11,90 @@ package apps;
  */
 public class Car {
     // definisi atribut
-    int id;
-    int no;
-    int color;
+    int id=0;
+    int available=0;
+    String no[];
+    String color[];
 
-    //define method park
-    void park(){
-        System.out.println(id +" is running...");
-        System.out.println("Speed: "+ no);
-    }
-
-    //define method leave
-    void leave(){
-        System.out.println(id +" is running...");
-    }
-    
     //define method init park lot
     void initLot(String val){
-        try{
-            id = Integer.parseInt(val);
-            System.out.println("Created a parking lot with "+id+" slots");
-        }catch (NumberFormatException ex) {
-            //handle exception here
-            System.out.println("Failed created a parking lot");
+        if(val.contains("0") || id!=0){
+            //tidak bisa insert 0 slot dan tidak bisa create lagi setelah lot terbentuk
+            System.out.println("Cannot create 0 parking lot or parking lot already created!");
+        }else{
+            try{
+                //hanya init jika input lot adalah int
+                id = Integer.parseInt(val);
+                no = new String[id];
+                color = new String[id];
+
+                System.out.println("Created a parking lot with "+id+" slots");
+            }catch (NumberFormatException ex) {
+                //handle exception here
+                System.out.println("Failed created a parking lot");
+            }
         }
     }
     
+    //define method park
+    void park(String car_no, String car_color){
+        int i;
+        for(i=0; i<id; i++){
+            if(no[i]==null && color[i]==null){
+                no[i] = car_no;
+                color[i] = car_color;
+                break;
+            }
+        }
+        if(i<id){
+            System.out.println("Allocated  slot number:" + (i+1));
+        }else{
+            System.out.println("Sorry, parking lot is full");
+        }
+    }
+
+    //define method leave
+    void leave(String value){
+        try{
+            int val = Integer.parseInt(value)-1;
+            no[val] = null;
+            color[val] = null;
+            System.out.println("Slot number "+value+" is free");
+        }catch(Exception ex){
+            //handle exception here
+            System.out.println("Failed freeing slot");
+        }
+    }
     
+    //define method status
+    void status(){
+        System.out.println("Slot No. \t Registration No \t Colour");
+        for(int i=0; i<id; i++){
+            if(no[i]!=null && color[i]!=null){
+                System.out.println((i+1) +" \t\t "+ no[i] +" \t\t "+ color[i]);
+            }
+        }
+    }
+    
+    //define registration no by color
+    void getNoByColor(String col){
+        System.out.println(col);
+        /*String listNo="";
+        for(int i=0; i<id; i++){
+            if(color[i].contains("col")){
+                listNo = listNo + no[i] + ",";
+            }
+        }
+        System.out.println(listNo);*/
+    }
+    
+    //get slot by car color
+    void getSlotByColor(String color){
+        
+    }
+    
+    //get slot by registered car no
+    void getSlotByNo(String no){
+        
+    }
 }
