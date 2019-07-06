b2834e63053df7659a3474d8f38cafca92960cf6
Dataon <Dataon@LAPBLZ018.ASJ211304-0000401>
2019-07-06 14:27:02 +0700

    open cmd on running program

--- Apps/src/apps/Apps.java
+++ Apps/src/apps/Apps.java
@@ -16,6 +16,19 @@ public class Apps {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try
+        { 
+            // Just one line and you are done !  
+            // We have given a command to start cmd 
+            // /K : Carries out command specified by string 
+           Runtime.getRuntime().exec(new String[] {"cmd", "/K", "Start"}); 
+  
+        } 
+        catch (Exception e) 
+        { 
+            System.out.println("HEY Buddy ! U r Doing Something Wrong "); 
+            e.printStackTrace(); 
+        } 
     }
     
 }
