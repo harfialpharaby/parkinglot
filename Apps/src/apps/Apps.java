335fd7a54ed542f7ddf0904448b221bf21c8027c
Dataon <Dataon@LAPBLZ018.ASJ211304-0000401>
2019-07-06 16:28:41 +0700

    include car class

--- Apps/src/apps/Apps.java
+++ Apps/src/apps/Apps.java
@@ -5,6 +5,13 @@
  */
 package apps;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Scanner;
+
 /**
  *
  * @author Dataon
@@ -14,21 +21,20 @@ public class Apps {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // TODO code application logic here
-        try
-        { 
-            // Just one line and you are done !  
-            // We have given a command to start cmd 
-            // /K : Carries out command specified by string 
-           Runtime.getRuntime().exec(new String[] {"cmd", "/K", "Start"}); 
-  
-        } 
-        catch (Exception e) 
-        { 
-            System.out.println("HEY Buddy ! U r Doing Something Wrong "); 
-            e.printStackTrace(); 
-        } 
+        Scanner myObj = new Scanner(System.in);
+        String Commands;
+        
+        // membuat objek player
+        Car parking = new Car();
+
+        // Enter Commands and press Enter
+        Commands = myObj.nextLine();
+        
+        if(Commands.contains(" ")){
+            Commands = Commands.substring(0, Commands.indexOf(" "));
+            System.out.println("Commands is: " + Commands + " and length: "+ Commands.length());
+        }
     }
-    
 }
